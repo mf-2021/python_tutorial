a7421d4fb4ab668b80416c872a8d7c23237ae316
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-11 19:34:04 +0900

    [Finish]3.2

--- python_tutorial_3-1-3.py
+++ python_tutorial_3-1-3.py
@@ -32,3 +32,18 @@ x = [a, n]
 print(x)
 print(x[0])
 print(x[0][1])
+
+# フィボナッチ級数
+# 2項の和により次項が定まる
+a, b = 0, 1
+while b < 10:
+    print(b)
+    a, b = b, a + b
+
+i = 256 * 256
+print('The value of i is', i)
+
+a, b = 0, 1
+while b < 1000:
+    print(b, end=',')
+    a, b = b, a + b
