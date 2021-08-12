616f03ccee014fb056ecf8daf27c7df60291a50e
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-12 14:54:20 +0900

    [Finish]4.3 range() 関数

--- python_tutorial_4.py
+++ python_tutorial_4.py
@@ -11,12 +11,29 @@
 #     print('もっと')
 
 # 4.2　for文
-words = ['cat', 'window', 'defenstrate']
-for w in words:
-    print(w, len(w))
+# words = ['cat', 'window', 'defenstrate']
+# for w in words:
+#     print(w, len(w))
 
-for w in words[:]:
-    if len(w) > 6:
-        words.insert(0, w)
+# for w in words[:]:
+#     if len(w) > 6:
+#         words.insert(0, w)
 
-print(words)
+# print(words)
+
+# 4.3 range() 関数
+# for i in range(5):
+#     print(i)
+    
+
+# for i in range(5, 10):
+#     print(i)
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+print(len(a))
+for i in range(len(a)):
+    print(i, a[i])
+
+print(range(10))
+
+print(list(range(5)))
