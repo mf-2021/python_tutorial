51feb6bf29d87a4489bd52b6bab5714939aecc9d
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-12 15:33:48 +0900

    [Finish]4.4 break文とcontinue文、ループにおけるelse節

--- python_tutorial_4.py
+++ python_tutorial_4.py
@@ -29,11 +29,27 @@
 # for i in range(5, 10):
 #     print(i)
 
-a = ['Mary', 'had', 'a', 'little', 'lamb']
-print(len(a))
-for i in range(len(a)):
-    print(i, a[i])
+# a = ['Mary', 'had', 'a', 'little', 'lamb']
+# print(len(a))
+# for i in range(len(a)):
+#     print(i, a[i])
 
-print(range(10))
+# print(range(10))
 
-print(list(range(5)))
+# print(list(range(5)))
+
+
+# 4.4 break文とcontinue文、ループにおけるelse節
+# for n in range(2, 10):
+#     for x in range(2, n):
+#         if n % x == 0:
+#             print(n, 'equals', x, '*', n // x)
+#             break
+#     else:
+#         print(n, 'is a prime number')
+
+for num in range(2, 10):
+    if num % 2 == 0:
+        print("Found an even number", num)
+        continue
+    print("Found a number", num)
