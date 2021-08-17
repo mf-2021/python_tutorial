9808a1321c2c9019f4507f644480545e9adb7a14
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-17 21:13:46 +0900

    [Finish]4.7.2

--- python_tutorial_4.py
+++ python_tutorial_4.py
@@ -118,20 +118,51 @@ def ask_ok(prompt, retries=4, complaint='Yes or no, please!'):
 # i = 6
 # f()
 
-def f(a, L=[]):
-    L.append(a)
-    return L
+# def f(a, L=[]):
+#     L.append(a)
+#     return L
 
-print(f(1))
-print(f(2))
-print(f(3))
+# print(f(1))
+# print(f(2))
+# print(f(3))
 
-def f(a, L=None):
-    if L is None:
-        L = []
-    L.append(a)
-    return L
+# def f(a, L=None):
+#     if L is None:
+#         L = []
+#     L.append(a)
+#     return L
 
-print(f(1, [5]))
-print(f(2))
-print(f(3))
+# print(f(1, [5]))
+# print(f(2))
+# print(f(3))
+
+
+# 4.7.2　キーワード引数
+
+# def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+#     print("-- This parrot wouldn\'t", action, end=' ')
+#     print("if you put", voltage, "volts through it.")
+#     print("-- Lovely plumage, the", type)
+#     print("-- It\'s", state, "!")
+
+# # parrot(1000)
+# # parrot(voltage=1000)
+# # parrot(voltage=10000000, action='VOOOOOM')
+# parrot('a million', 'bereft of life', 'jump')
+# parrot('a thousand', state='pushing up the daises')
+
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I\'m sorry, we\'re all out of", kind)
+    # for arg in arguments:
+    #     print(arg)
+    print("-" * 40)
+    keys = sorted(keywords.keys())
+    for kw in keys:
+        print(kw, ":", keywords[kw])
+
+cheeseshop("Limburger", "It\'s very runny, sir.",
+           "It\'s really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
