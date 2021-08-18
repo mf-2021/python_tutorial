7e7b2c021c5bfa56de703433f462b19bdecb0f13
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-18 19:15:30 +0900

    [Finish]5.1.3

--- py_tuto_5.py
+++ py_tuto_5.py
@@ -39,11 +39,58 @@
 
 
 # 5.1.2 リストをキューとして使う
-from collections import deque
-queue = deque(["Eric", "John", "Michael"])
-queue.append("Terry")
-print(queue)
-queue.append("Graham")
-print(queue.popleft())
-print(queue.popleft())
-print(queue)
+# from collections import deque
+# queue = deque(["Eric", "John", "Michael"])
+# queue.append("Terry")
+# print(queue)
+# queue.append("Graham")
+# print(queue.popleft())
+# print(queue.popleft())
+# print(queue)
+
+
+
+# 5.1.3 リスト内包
+# squares = []
+# for x in range(10):
+#     squares.append(x**2)
+
+# print(squares)
+
+# squares = list(map(lambda x: x**2, range(10)))
+# print(squares)
+
+# squares = [x**2 for x in range(10)]
+# print(squares)
+
+# print([(x, y) for x in [1, 2, 3] for y in [3, 1, 4] if x != y])
+
+# combs = []
+# for x in [1, 2, 3]:
+#     for y in [3, 1, 4]:
+#         if x != y:
+#             combs.append((x, y))
+
+# print(combs)
+
+
+# vec = [-4, -2, 0, 2, 4]
+# print([x*2 for x in vec])
+
+# print([x for x in vec if x >= 0])
+
+# print([abs(x) for x in vec])
+
+
+# freshfruit = [' banana ', ' loganberry ', 'passion fruit ']
+# print([weapon.strip() for weapon in freshfruit])
+
+# print([(x, x**2) for x in range(6)])
+
+# # print(x, x**2 for x in range(6))
+
+# vec = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# print([num for elem in vec for num in elem])
+
+from math import pi
+print([str(round(pi, i)) for i in range(1, 6)])
