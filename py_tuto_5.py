1a8986023468f1cc3bd0ae56c16b2974702f1009
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-18 18:09:32 +0900

    [Finish]5.1.1

--- py_tuto_5.py
+++ py_tuto_5.py
@@ -1,21 +1,37 @@
 # 5.1　リストについての補足
-a = [66.25, 333, 333, 1, 1234.5]
-print(a.count(333), a.count(66.25), a.count('x'))
+# a = [66.25, 333, 333, 1, 1234.5]
+# print(a.count(333), a.count(66.25), a.count('x'))
 
-a.insert(2, -1)
-a.append(333)
-print(a)
+# a.insert(2, -1)
+# a.append(333)
+# print(a)
 
-print(a.index(333))
+# print(a.index(333))
 
-a.remove(333)
-print(a)
+# a.remove(333)
+# print(a)
 
-a.reverse()
-print(a)
+# a.reverse()
+# print(a)
 
-a.sort()
-print(a)
+# a.sort()
+# print(a)
 
-a.pop()
-print(a)
+# a.pop()
+# print(a)
+
+
+
+# 5.1.1 リストをスタックとして使う
+stack = [3, 4, 5]
+stack.append(6)
+stack.append(7)
+print(stack)
+
+print(stack.pop())
+print(stack)
+
+print(stack.pop())
+
+print(stack.pop())
+print(stack)
