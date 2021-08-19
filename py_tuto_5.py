a971b4dfc74bab49bc72e2db9403664df414ecb3
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-19 19:34:58 +0900

    [Finish]5.1.4

--- py_tuto_5.py
+++ py_tuto_5.py
@@ -92,5 +92,34 @@
 # vec = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 # print([num for elem in vec for num in elem])
 
-from math import pi
-print([str(round(pi, i)) for i in range(1, 6)])
+# from math import pi
+# print([str(round(pi, i)) for i in range(1, 6)])
+
+
+
+# 5.1.4 入れ子のリスト内包
+matrix = [
+    [1, 2, 3, 4], 
+    [5, 6, 7, 8], 
+    [9, 10, 11, 12]
+]
+
+# print([[row[i] for row in matrix] for i in range(4)])
+
+# transposed = []
+# for i in range(4):
+#     transposed.append([row[i] for row in matrix])
+
+# print(transposed)
+
+
+# transposed = []
+# for i in range(4):
+#     transposed_row = []
+#     for row in matrix:
+#         transposed_row.append(row[i])
+#     transposed.append(transposed_row)
+
+# print(transposed)
+
+print(list(zip(*matrix)))
