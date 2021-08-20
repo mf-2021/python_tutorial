64dafc7dea5d8b8d76b06f9b633a6289cd57cc29
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-20 19:05:41 +0900

    [Finish]5.3

--- py_tuto_5.py
+++ py_tuto_5.py
@@ -127,15 +127,38 @@
 
 
 # del文
-a = [-1, 1, 66.25, 333, 333, 1234.5]
-del a[0]
-print(a)
+# a = [-1, 1, 66.25, 333, 333, 1234.5]
+# del a[0]
+# print(a)
 
-del a[2:4]
-print(a)
+# del a[2:4]
+# print(a)
 
-del a[:]
-print(a)
+# del a[:]
+# print(a)
 
-del a
-print(a)
+# del a
+# print(a)
+
+
+
+# 5.3　タブルとシーケンス
+# t = 12345, 54321, 'hello!'
+# print(t[0])
+# print(t)
+
+# u = t, (1, 2, 3, 4, 5)
+# print(u)
+
+# # t[0] = 88888
+
+# v = ([1, 2, 3], [3, 2, 1])
+# print(v)
+
+empty = ()
+singleton = 'hello',
+print(len(empty))
+
+print(len(singleton))
+
+print(singleton)
