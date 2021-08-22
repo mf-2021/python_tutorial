501f59afdf26ec85e99752bb2428c998113c25a6
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-22 15:40:13 +0900

    [Start]5.6

--- py_tuto_5.py
+++ py_tuto_5.py
@@ -207,8 +207,23 @@
 
 # print('jack' not in tel)
 
-print(dict([('sape', 4139), ('guido', 4127), ('jack', 4098)]))
+# print(dict([('sape', 4139), ('guido', 4127), ('jack', 4098)]))
 
-print({x: x**2 for x in (2, 4, 6)})
+# print({x: x**2 for x in (2, 4, 6)})
 
-print(dict(sape=4139, guido=4127, jack=4098))
+# print(dict(sape=4139, guido=4127, jack=4098))
+
+
+
+# 5.6　ループのテクニック
+# knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+# for k, v in knights.items():
+#     print(k, v)
+
+# for i, v in enumerate(['tic', 'tac', 'toe']):
+#     print(i, v)
+
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+    print('What is your {0}? It is {1}.'.format(q, a))
