90a426f812e0c92c722bf0911ed2febbcc0e9ae7
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-25 13:56:52 +0900

    [Finish]7.2.1

--- py_tuto_7.py
+++ py_tuto_7.py
@@ -63,3 +63,23 @@
 # import math
 # print('πの値はおよそ %5.3fである。' % math.pi)
 
+
+
+# ファイルの読み書き
+# f = open('test.txt', 'r')
+# print(f.read())
+
+# f = open('test.txt', 'w')
+# f.write('This is a test.\n')
+
+# value = ('the answer', 42)
+# s = str(value)
+# f.write(s)
+
+f = open('test.txt', 'rb+')
+f.write(b'0123456789abcdef')
+
+print(f.seek(5))
+print(f.read(1))
+print(f.seek(-3, 2))
+f.read(1)
