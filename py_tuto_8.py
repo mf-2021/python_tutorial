a33ed764c429b7dc2354652750db42015f577f14
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-25 15:38:00 +0900

    [Finish]8.5

--- py_tuto_8.py
+++ py_tuto_8.py
@@ -61,8 +61,54 @@
 
 
 
-try:
-    raise NameError('HiThere')
-except NameError:
-    print('例外が飛んでった！')
-    raise
+# try:
+#     raise NameError('HiThere')
+# except NameError:
+#     print('例外が飛んでった！')
+#     raise
+
+
+
+# class MyError(Exception):
+#     def __int__(self, value):
+#         self.value = value
+#     def __str__(self):
+#         return repr(self.value)
+
+# try:
+#     raise MyError(2*2)
+# except MyError as e:
+#     print('My exception occured, value:', e.value)
+
+
+
+class Error(Exception):
+    """このモジュールの例外のベースクラス"""
+    pass
+
+class InputError(Error):
+    """入力エラーで送出される例外
+    
+    属性：
+        expression -- エラーが起きた入力式
+        message -- エラーの説明
+    """
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
+
+class TransitionError(Error):
+    """許可されない状態繊維を起こそうとする操作があれば送出される
+    
+    属性：
+        previous -- 遷移前の状態
+        next -- 移ろうとした状態
+        message -- その遷移がなぜ許可されないかの説明
+    """
+
+    def __init__(self, previous, next, message):
+        self.previous = previous
+        self.next = next
+        self.message = message
+
