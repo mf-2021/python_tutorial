c3a2b9858be6e13b5172736372ebf8b4d6b8c7bf
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-26 16:54:27 +0900

    [Finish]9.3.2

--- py_tuto_9.py
+++ py_tuto_9.py
@@ -1,19 +1,42 @@
-def scope_test():
-    def do_local():
-        spam = "local spam"
-    def do_nonlocal():
-        nonlocal spam
-        spam = "nonlacal spam"
-    def do_global():
-        global spam
-        spam = "global spam"
-    spam = "test spam"
-    do_local()
-    print("After local assignment:", spam)
-    do_nonlocal()
-    print("Afrer nonlocal assignment:", spam)
-    do_global()
-    print("After global assignment:", spam)
+# def scope_test():
+#     def do_local():
+#         spam = "local spam"
+#     def do_nonlocal():
+#         nonlocal spam
+#         spam = "nonlacal spam"
+#     def do_global():
+#         global spam
+#         spam = "global spam"
+#     spam = "test spam"
+#     do_local()
+#     print("After local assignment:", spam)
+#     do_nonlocal()
+#     print("Afrer nonlocal assignment:", spam)
+#     do_global()
+#     print("After global assignment:", spam)
 
-scope_test()
-print("In global scope;", spam)
+# scope_test()
+# print("In global scope;", spam)
+
+
+
+# class MyClass:
+#     """A simple example class"""
+#     i = 12345
+#     def f(self):
+#         return 'hello world'
+
+#     x = MyClass()
+
+#     def __init__(self):
+#         self.data = []
+
+
+
+class Complex:
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
+        
+x = Complex(3.0, -4.5)
+print(x.r, x.i)
