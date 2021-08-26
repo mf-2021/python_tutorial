3136ef95bf3d8e29a4d00cda6366c8481edd3b2a
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-26 17:06:53 +0900

    [Finish]9.3.3

--- py_tuto_9.py
+++ py_tuto_9.py
@@ -26,17 +26,23 @@
 #     def f(self):
 #         return 'hello world'
 
-#     x = MyClass()
-
 #     def __init__(self):
 #         self.data = []
 
+#     x = MyClass()
+
+#     x.counter = 1
+#     while x.counter < 10:
+#         x.counter = x.counter * 2
+#     print(x.counter)
+#     del x.counter
 
 
-class Complex:
-    def __init__(self, realpart, imagpart):
-        self.r = realpart
-        self.i = imagpart
-        
-x = Complex(3.0, -4.5)
-print(x.r, x.i)
+# class Complex:
+#     def __init__(self, realpart, imagpart):
+#         self.r = realpart
+#         self.i = imagpart
+
+# x = Complex(3.0, -4.5)
+# print(x.r, x.i)
+
