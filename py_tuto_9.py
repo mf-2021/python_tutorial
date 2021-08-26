be03b152e47747c11bcbccbb876273469443fb81
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-26 22:04:09 +0900

    [Finish]9.9

--- py_tuto_9.py
+++ py_tuto_9.py
@@ -184,19 +184,64 @@
 
 
 
-class B(Exception):
-    pass
-class C(B):
-    pass
-class D(C):
-    pass
+# class B(Exception):
+#     pass
+# class C(B):
+#     pass
+# class D(C):
+#     pass
 
-for cls in [B, C, D]:
-    try:
-        raise cls()
-    except D:
-        print("D")
-    except C:
-        print("C")
-    except B:
-        print("B")
+# for cls in [B, C, D]:
+#     try:
+#         raise cls()
+#     except D:
+#         print("D")
+#     except C:
+#         print("C")
+#     except B:
+#         print("B")
+
+
+
+# for element in [1, 2, 3]:
+#     print(element)
+# for element in (1, 2, 3):
+#     print(element)
+# for key in {'one': 1, 'two': 2}:
+#     print(key)
+# for char in "123":
+#     print(char)
+# for line in open("test.txt"):
+    # print(line, end=" ")
+
+
+
+# s = 'abc'
+# it = iter(s)
+# print(it)
+
+# print(next(it))
+# print(next(it))
+# print(next(it))
+# print(next(it))
+
+
+
+class Reverse:
+    "シーケンスを逆順にループする反復子"
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+rev = Reverse('spam')
+print(iter(rev))
+
+for char in rev:
+    print(char)
