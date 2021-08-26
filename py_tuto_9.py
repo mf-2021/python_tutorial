6b3acd317d5c0b03a492272719bd937c701e0f58
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-26 22:12:00 +0900

    [Finish]9.10

--- py_tuto_9.py
+++ py_tuto_9.py
@@ -227,21 +227,30 @@
 
 
 
-class Reverse:
-    "シーケンスを逆順にループする反復子"
-    def __init__(self, data):
-        self.data = data
-        self.index = len(data)
-    def __iter__(self):
-        return self
-    def __next__(self):
-        if self.index == 0:
-            raise StopIteration
-        self.index = self.index - 1
-        return self.data[self.index]
+# class Reverse:
+#     "シーケンスを逆順にループする反復子"
+#     def __init__(self, data):
+#         self.data = data
+#         self.index = len(data)
+#     def __iter__(self):
+#         return self
+#     def __next__(self):
+#         if self.index == 0:
+#             raise StopIteration
+#         self.index = self.index - 1
+#         return self.data[self.index]
 
-rev = Reverse('spam')
-print(iter(rev))
+# rev = Reverse('spam')
+# print(iter(rev))
 
-for char in rev:
+# for char in rev:
+#     print(char)
+
+
+
+def reverse(data):
+    for index in range(len(data)-1, -1, -1):
+        yield data[index]
+
+for char in reverse('golf'):
     print(char)
